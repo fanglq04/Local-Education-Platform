5d754450ed6ac94e27dd6a84417a3834b110ba26
fanglq04 <fanglq04@126.com>
2016-07-18 20:25:22 +0800

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# 地方教育平台（Larval 5.2 RBAC）
+
+## 说明
+
+基于Laravel 5.2 的 RBAC 开发的本地教育平台
+
+
+## 安装
+
+- git clone 到本地
+- 执行 `composer install`
+- 配置 **.env** 中数据库连接信息
+- 执行 `php artisan db:seed`
+- 执行 `php artisan serve`
+- 默认后台账号:admin@admin.com 密码:admin
---
+++ app/Http/routes.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Routes File
+|--------------------------------------------------------------------------
+|
+| Here is where you will register all of the routes in an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+
+Route::group(['middleware' => 'web'], function () {
+    Route::auth();
+
+    Route::get('/home', 'HomeController@index');
+});
+
+
+Route::group(['middleware' => ['web'], 'namespace' => 'Admin', 'prefix' => 'admin'], function () {
+    Route::auth();
+
+    Route::get('/home', ['as' => 'admin.home', 'uses' => 'HomeController@index']);
+    Route::resource('admin_user', 'AdminUserController');
+    Route::post('admin_user/destroyall',['as'=>'admin.admin_user.destroy.all','uses'=>'AdminUserController@destroyAll']);
+    Route::resource('role', 'RoleController');
+    Route::post('role/destroyall',['as'=>'admin.role.destroy.all','uses'=>'RoleController@destroyAll']);
+    Route::get('role/{id}/permissions',['as'=>'admin.role.permissions','uses'=>'RoleController@permissions']);
+    Route::post('role/{id}/permissions',['as'=>'admin.role.permissions','uses'=>'RoleController@storePermissions']);
+    Route::resource('permission', 'PermissionController');
+    Route::post('permission/destroyall',['as'=>'admin.permission.destroy.all','uses'=>'PermissionController@destroyAll']);
+    Route::resource('blog', 'BlogController');
+});
+
+
+Route::get('/admin', function () {
+    return view('admin.welcome');
+});
+
+Route::group(['middleware' => 'web'], function () {
+    Route::auth();
+
+    Route::get('/home', 'HomeController@index');
+});
