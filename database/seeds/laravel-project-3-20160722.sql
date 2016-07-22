3d68e1369939be4edbeb5781c79ed757384711b0
fanglq04 <fanglq04@126.com>
2016-07-22 17:16:06 +0800

    更新本次sql

--- database/seeds/laravel-project-3-20160722.sql
+++ database/seeds/laravel-project-3-20160722.sql
@@ -41,7 +41,7 @@ CREATE TABLE IF NOT EXISTS `admin_users` (
 -- 正在导出表  laravel-project-3.admin_users 的数据：~1 rows (大约)
 /*!40000 ALTER TABLE `admin_users` DISABLE KEYS */;
 INSERT INTO `admin_users` (`id`, `name`, `email`, `password`, `is_super`, `remember_token`, `created_at`, `updated_at`) VALUES
-	(1, 'admin', 'admin@admin.com', '$2y$10$GBKiY/ngDVpe1iHwlTem3e0fbNrnv1sRLGcj4wT1isK0gbzY4oQoC', 1, 'kwXV90VBYWdZxWomfNrpZHuaaatlihTUtpsuBCE38W9O4NiN5xuw4zNqYJ22', '2016-07-18 20:47:11', '2016-07-18 12:47:11');
+	(1, 'admin', 'admin@admin.com', '$2y$10$GBKiY/ngDVpe1iHwlTem3e0fbNrnv1sRLGcj4wT1isK0gbzY4oQoC', 1, 'jPGgBSGYXlD58xgBNTP9VySHNOdpCo1635DwfCHf7EKAkvZARX8wYD95qW1G', '2016-07-22 11:37:59', '2016-07-22 03:37:59');
 /*!40000 ALTER TABLE `admin_users` ENABLE KEYS */;
 
 
@@ -113,10 +113,6 @@ CREATE TABLE IF NOT EXISTS `members` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(10) unsigned NOT NULL,
   `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `gender` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1男2女',
-  `avatar` varchar(250) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `authable` tinyint(1) NOT NULL DEFAULT '0' COMMENT '1已认证0未认证',
-  `status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '0拉黑1正常',
   `passport_front_images` varchar(250) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '认证照片身份证正面',
   `passport_back_images` varchar(250) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '认证照片身份证反面',
   `passport_handheld_id_images` varchar(250) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '认证照片手拿身份证',
@@ -129,8 +125,8 @@ CREATE TABLE IF NOT EXISTS `members` (
 
 -- 正在导出表  laravel-project-3.members 的数据：~1 rows (大约)
 /*!40000 ALTER TABLE `members` DISABLE KEYS */;
-INSERT INTO `members` (`id`, `user_id`, `name`, `gender`, `avatar`, `authable`, `status`, `passport_front_images`, `passport_back_images`, `passport_handheld_id_images`, `created_at`, `updated_at`) VALUES
-	(1, 3, '张小星家长', 1, NULL, 0, 1, '', '', '', '2016-07-20 10:41:54', '0000-00-00 00:00:00');
+INSERT INTO `members` (`id`, `user_id`, `name`, `passport_front_images`, `passport_back_images`, `passport_handheld_id_images`, `created_at`, `updated_at`) VALUES
+	(1, 3, '张小星家长', '', '', '', '2016-07-20 10:41:54', '0000-00-00 00:00:00');
 /*!40000 ALTER TABLE `members` ENABLE KEYS */;
 
 
@@ -161,9 +157,6 @@ CREATE TABLE IF NOT EXISTS `organizations` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(10) unsigned NOT NULL,
   `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `gender` tinyint(1) NOT NULL DEFAULT '1',
-  `authable` tinyint(4) NOT NULL DEFAULT '0',
-  `status` tinyint(1) NOT NULL DEFAULT '1',
   `corporate_id_images` varchar(250) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '法人身份证照',
   `business license_images` varchar(250) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '企业营业执照照',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -175,8 +168,8 @@ CREATE TABLE IF NOT EXISTS `organizations` (
 
 -- 正在导出表  laravel-project-3.organizations 的数据：~1 rows (大约)
 /*!40000 ALTER TABLE `organizations` DISABLE KEYS */;
-INSERT INTO `organizations` (`id`, `user_id`, `name`, `gender`, `authable`, `status`, `corporate_id_images`, `business license_images`, `created_at`, `updated_at`) VALUES
-	(2, 2, '滁州龙兴教育培训机构', 1, 0, 1, NULL, NULL, '2016-07-19 10:53:24', '0000-00-00 00:00:00');
+INSERT INTO `organizations` (`id`, `user_id`, `name`, `corporate_id_images`, `business license_images`, `created_at`, `updated_at`) VALUES
+	(2, 2, '滁州龙兴教育培训机构', NULL, NULL, '2016-07-19 10:53:24', '0000-00-00 00:00:00');
 /*!40000 ALTER TABLE `organizations` ENABLE KEYS */;
 
 
@@ -208,16 +201,16 @@ CREATE TABLE IF NOT EXISTS `permissions` (
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`),
   UNIQUE KEY `permissions_name_unique` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=59 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=64 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- 正在导出表  laravel-project-3.permissions 的数据：~26 rows (大约)
+-- 正在导出表  laravel-project-3.permissions 的数据：~31 rows (大约)
 /*!40000 ALTER TABLE `permissions` DISABLE KEYS */;
 INSERT INTO `permissions` (`id`, `fid`, `icon`, `name`, `display_name`, `description`, `is_menu`, `sort`, `created_at`, `updated_at`) VALUES
-	(20, 0, 'edit', '#-1456129983', '系统设置', '', 1, 100, '2016-02-22 16:33:03', '2016-02-22 16:33:03'),
+	(20, 0, 'edit', '#-1456129983', '系统设置', '', 1, 100, '2016-07-22 11:37:31', '2016-02-22 16:33:03'),
 	(21, 20, '', 'admin.admin_user.index', '用户权限', '查看后台用户列表', 1, 0, '2016-02-18 15:56:26', '2016-02-18 15:56:26'),
 	(22, 20, '', 'admin.admin_user.create', '创建后台用户', '页面', 0, 0, '2016-02-23 11:48:18', '2016-02-23 11:48:18'),
 	(35, 0, 'home', 'admin.home', 'Dashboard', '后台首页', 1, 0, '2016-02-22 16:32:40', '2016-02-22 16:32:40'),
-	(36, 0, ' fa-laptop', '#-1456132007', '博客管理', '', 1, 0, '2016-02-22 17:06:47', '2016-02-22 17:06:47'),
+	(36, 0, 'fa-laptop', '#-1456132007', '博客管理', '', 1, 0, '2016-07-22 10:54:54', '2016-02-22 17:06:47'),
 	(37, 36, '', 'admin.blog.index', '博客列表', '', 1, 0, '2016-02-22 17:15:48', '2016-02-22 17:15:48'),
 	(38, 20, '', 'admin.admin_user.store', '保存新建后台用户', '操作', 0, 0, '2016-02-23 11:48:52', '2016-02-23 11:48:52'),
 	(39, 20, '', 'admin.admin_user.destroy', '删除后台用户', '操作', 0, 0, '2016-02-23 11:49:09', '2016-02-23 11:49:09'),
@@ -238,7 +231,12 @@ INSERT INTO `permissions` (`id`, `fid`, `icon`, `name`, `display_name`, `descrip
 	(55, 20, '', 'admin.role.update', '保存编辑角色', '操作', 0, 0, '2016-02-23 11:58:50', '2016-02-23 11:58:50'),
 	(56, 20, '', 'admin.role.permissions', '角色权限设置', '', 0, 0, '2016-02-23 11:59:26', '2016-02-23 11:59:26'),
 	(57, 20, '', 'admin.role.destroy', '角色删除', '操作', 0, 0, '2016-02-23 11:59:49', '2016-02-23 11:59:49'),
-	(58, 20, '', 'admin.role.destory.all', '批量删除角色', '', 0, 0, '2016-02-23 12:01:58', '2016-02-23 12:01:58');
+	(58, 20, '', 'admin.role.destory.all', '批量删除角色', '', 0, 0, '2016-02-23 12:01:58', '2016-02-23 12:01:58'),
+	(59, 0, 'fa-male', '#-1469152218', '用户管理', '用户管理组', 1, 0, '2016-07-22 10:54:59', '2016-07-22 01:50:18'),
+	(60, 59, '', 'admin.member.index', '用户列表', '页面', 1, 0, '2016-07-22 11:04:44', '2016-07-22 02:59:38'),
+	(61, 59, '', 'admin.member.edit', '编辑用户', '页面', 0, 0, '2016-07-22 11:04:49', '2016-07-22 02:28:53'),
+	(62, 59, '', 'admin.member.forbidden', '禁用用户', '操作', 0, 0, '2016-07-22 11:04:52', '2016-07-22 02:29:26'),
+	(63, 59, '', 'admin.member.detail', '详细信息', '页面', 0, 0, '2016-07-22 11:04:55', '2016-07-22 02:29:47');
 /*!40000 ALTER TABLE `permissions` ENABLE KEYS */;
 
 
@@ -252,7 +250,7 @@ CREATE TABLE IF NOT EXISTS `permission_role` (
   CONSTRAINT `permission_role_role_id_foreign` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- 正在导出表  laravel-project-3.permission_role 的数据：~32 rows (大约)
+-- 正在导出表  laravel-project-3.permission_role 的数据：~37 rows (大约)
 /*!40000 ALTER TABLE `permission_role` DISABLE KEYS */;
 INSERT INTO `permission_role` (`permission_id`, `role_id`) VALUES
 	(20, 10),
@@ -281,6 +279,11 @@ INSERT INTO `permission_role` (`permission_id`, `role_id`) VALUES
 	(56, 10),
 	(57, 10),
 	(58, 10),
+	(59, 10),
+	(60, 10),
+	(61, 10),
+	(62, 10),
+	(63, 10),
 	(20, 12),
 	(21, 12),
 	(22, 12),
@@ -296,10 +299,6 @@ CREATE TABLE IF NOT EXISTS `personals` (
   `user_id` int(10) unsigned NOT NULL,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `description` longtext COLLATE utf8_unicode_ci,
-  `gender` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1男2女',
-  `avatar` varchar(250) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `authable` tinyint(1) NOT NULL DEFAULT '0' COMMENT '1已认证0未认证',
-  `status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '0拉黑1正常',
   `passport_front_images` varchar(250) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '认证照片身份证正面',
   `passport_back_images` varchar(250) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '认证照片身份证反面',
   `passport_handheld_id_images` varchar(250) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '认证照片手拿身份证',
@@ -312,8 +311,8 @@ CREATE TABLE IF NOT EXISTS `personals` (
 
 -- 正在导出表  laravel-project-3.personals 的数据：~1 rows (大约)
 /*!40000 ALTER TABLE `personals` DISABLE KEYS */;
-INSERT INTO `personals` (`id`, `user_id`, `name`, `description`, `gender`, `avatar`, `authable`, `status`, `passport_front_images`, `passport_back_images`, `passport_handheld_id_images`, `created_at`, `updated_at`) VALUES
-	(3, 1, '张老师', '个人金牌家教', 1, '', 0, 0, '', '', '', '2016-07-20 10:36:07', '0000-00-00 00:00:00');
+INSERT INTO `personals` (`id`, `user_id`, `name`, `description`, `passport_front_images`, `passport_back_images`, `passport_handheld_id_images`, `created_at`, `updated_at`) VALUES
+	(3, 1, '张老师', '个人金牌家教', '', '', '', '2016-07-20 10:36:07', '0000-00-00 00:00:00');
 /*!40000 ALTER TABLE `personals` ENABLE KEYS */;
 
 
@@ -403,11 +402,12 @@ CREATE TABLE IF NOT EXISTS `tag_user` (
   CONSTRAINT `FK_tag_user_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 正在导出表  laravel-project-3.tag_user 的数据：~1 rows (大约)
+-- 正在导出表  laravel-project-3.tag_user 的数据：~6 rows (大约)
 /*!40000 ALTER TABLE `tag_user` DISABLE KEYS */;
 INSERT INTO `tag_user` (`tag_id`, `user_id`) VALUES
 	(2, 1),
 	(3, 1),
+	(2, 2),
 	(4, 2),
 	(5, 2),
 	(6, 2);
@@ -419,21 +419,27 @@ CREATE TABLE IF NOT EXISTS `users` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `mobile` char(11) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '手机号',
   `type` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1用户-家长学生2培训机构3个人培训班',
   `password` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
   `remember_token` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `gender` tinyint(1) NOT NULL DEFAULT '0' COMMENT '1男2女',
+  `authable` tinyint(1) NOT NULL DEFAULT '0' COMMENT '1已认证0未认证',
+  `avatar` varchar(250) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '头像',
+  `status` tinyint(1) DEFAULT NULL COMMENT '0拉黑1正常',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `lastlogin_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_email_unique` (`email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- 正在导出表  laravel-project-3.users 的数据：~3 rows (大约)
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` (`id`, `name`, `email`, `type`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-	(1, 'test1', 'test1@admin.com', 3, '$2y$10$osDZZ10QibYuAmyQm4i0.OYMWn268cW/0rrxgcqi.e6HRU3gQvaDy', 'XNVvrMBuYwc6RY6dibjLJH9QjhNDk7eTzmZDv1RAP7kPEeHeefzVfgh5eeLZ', '2016-07-21 13:55:46', '2016-07-21 02:18:59'),
-	(2, 'test2', 'test2@admin.com', 2, '$2y$10$2ATviDVWAvaFMDXdaYuYke.RkP.tOGCjf2I0Sy0KIpEQCwUX3CG.2', '4jpLX88OcEKp22AngaCaU0DxMYycOk16sowFpOQbTQFu81CsHbhb8nI5Xxbn', '2016-07-20 10:41:29', '2016-07-20 02:41:29'),
-	(3, 'test3', 'test3@admin.com', 1, '$2y$10$C3oxZDRr4l5ykMdBKfewGu26tsG5Q/bBxmZ9DMIJFmtUDF8UaOpCi', 'XepCAhqwZLLcVx589939LulrCAq4gzJVjCmVgoJh6BRTTGpwrf4brVpO5XiW', '2016-07-21 13:55:45', '2016-07-19 01:51:32');
+INSERT INTO `users` (`id`, `name`, `email`, `mobile`, `type`, `password`, `remember_token`, `gender`, `authable`, `avatar`, `status`, `created_at`, `updated_at`, `lastlogin_at`) VALUES
+	(1, 'test1', 'test1@admin.com', NULL, 3, '$2y$10$osDZZ10QibYuAmyQm4i0.OYMWn268cW/0rrxgcqi.e6HRU3gQvaDy', 'XNVvrMBuYwc6RY6dibjLJH9QjhNDk7eTzmZDv1RAP7kPEeHeefzVfgh5eeLZ', 1, 0, NULL, 1, '2016-07-21 13:55:46', '2016-07-21 02:18:59', '0000-00-00 00:00:00'),
+	(2, 'test2', 'test2@admin.com', NULL, 2, '$2y$10$2ATviDVWAvaFMDXdaYuYke.RkP.tOGCjf2I0Sy0KIpEQCwUX3CG.2', '4jpLX88OcEKp22AngaCaU0DxMYycOk16sowFpOQbTQFu81CsHbhb8nI5Xxbn', 1, 0, NULL, 1, '2016-07-20 10:41:29', '2016-07-20 02:41:29', '0000-00-00 00:00:00'),
+	(3, 'test3', 'test3@admin.com', NULL, 1, '$2y$10$C3oxZDRr4l5ykMdBKfewGu26tsG5Q/bBxmZ9DMIJFmtUDF8UaOpCi', 'XepCAhqwZLLcVx589939LulrCAq4gzJVjCmVgoJh6BRTTGpwrf4brVpO5XiW', 1, 0, NULL, 1, '2016-07-21 13:55:45', '2016-07-19 01:51:32', '0000-00-00 00:00:00');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
